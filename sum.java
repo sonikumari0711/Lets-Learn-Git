a928ca813ef0eb12cc3e8c675ec82bdbb739cd6f
sonikumari0711 <sonikumaribooks@gmail.com>
2023-12-25 15:41:16 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String args[]){
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println("sum = "+a+b);
     }
 }
